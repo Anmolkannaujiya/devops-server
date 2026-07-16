2c2949440a3bbdc879eab2ee1473bd2a3433897c
anmolkannaujiya <anmolkannaujiya@outlook.com>
2026-07-16 11:50:43 +0530

    the last dance

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => {
-  res.json({ message: "ci-cd pipeline is working fine bhaiya gazab chiz hai" ,
-             message: "run ho ja bro"
+  res.json({ message1: "ci-cd pipeline is working fine bhaiya gazab chiz hai" ,
+             message2: "mind you final time"
   });
 });
 
